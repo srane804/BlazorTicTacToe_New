495ab24db55c45be5c460dfdac9739144c9bc094
Saurabh Rane <Saurabh@innovisk.global>
2025-01-03 16:35:32 +0530

    TicTacToe game created using Blazor and SignalR

---
+++ BlazorTicTacToe.Shared/TicTacToeGame.cs
@@ -0,0 +1,95 @@
+﻿namespace BlazorTicTacToe.Shared
+{
+    public class TicTacToeGame
+    {
+        public string PlayerXId { get; set; }
+        public string PlayerOId { get; set; }
+        public string? CurrentPlayerId { get; set; }
+        public string CurrentPlayerSymbol => CurrentPlayerId == PlayerXId ? "X" : "O";
+        public bool GameStarted { get; set; } = false;
+        public bool GameOver { get; set; } = false;
+        public bool IsDraw { get; set; } = false;
+        public string Winner { get; set; } = string.Empty;
+        public List<List<string>> Board { get; set; } = new List<List<string>>(3);
+
+        public TicTacToeGame() => InitializeBoard();
+
+        public void StartGame()
+        {
+            CurrentPlayerId = PlayerXId;
+            GameStarted = true;
+            GameOver = false;
+            IsDraw = false;
+            Winner = string.Empty;
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            Board.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                var row = new List<string>(3);
+                for (int j = 0; j < 3; j++)
+                {
+                    row.Add(string.Empty);
+                }
+                Board.Add(row);
+            }
+        }
+
+        public void TogglePlayer()
+        {
+            CurrentPlayerId = CurrentPlayerId == PlayerXId ? PlayerOId : PlayerXId;
+        }
+
+        public bool MakeMove(int row, int col, string playerId)
+        {
+            if (playerId != CurrentPlayerId
+                || row < 0 || row >= 3
+                || col < 0 || col >= 3
+                || Board[row][col] != string.Empty)
+            {
+                return false;
+            }
+
+            Board[row][col] = CurrentPlayerSymbol;
+            TogglePlayer();
+            return true;
+        }
+
+        public string CheckWinner()
+        {
+            // Check rows and columns
+            for (int i = 0; i < 3; i++)
+            {
+                if(!string.IsNullOrEmpty(Board[i][0]) && Board[i][0] == Board[i][1] && Board[i][1] == Board[i][2])
+                {
+                    return Board[i][0];
+                }
+
+                if (Board[0][i] == Board[1][i] && Board[1][i] == Board[2][i] && !string.IsNullOrEmpty(Board[0][i]))
+                {
+                    return Board[0][i];
+                }
+                    
+            }
+
+            // Check diagonals
+            if (Board[0][0] == Board[1][1] && Board[1][1] == Board[2][2] && !string.IsNullOrEmpty(Board[0][0]))
+                return Board[0][0];
+
+            if (Board[0][2] == Board[1][1] && Board[1][1] == Board[2][0] && !string.IsNullOrEmpty(Board[0][2]))
+                return Board[0][2];
+
+            // No winner
+            return string.Empty;
+        }
+
+        public bool CheckDraw()
+        {
+            return IsDraw = Board.All(row => row.All(cell => !string.IsNullOrEmpty(cell)));
+        }
+
+    }
+}
---
+++ BlazorTicTacToe/BlazorTicTacToe/Hubs/GameHub.cs
@@ -0,0 +1,78 @@
+﻿using BlazorTicTacToe.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BlazorTicTacToe.Hubs
+{
+    public class GameHub : Hub
+    {
+        private static readonly List<GameRoom> _rooms = [];
+
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Player with Id '{Context.ConnectionId}' connected");
+            await Clients.Caller.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
+        }
+
+        public async Task<GameRoom> CreateRoomAsync(string name, string playerName)
+        {
+            string roomId = Guid.NewGuid().ToString();
+            var room = new GameRoom(roomId, name);
+            _rooms.Add(room);
+
+            var newPlayer = new Player(Context.ConnectionId.ToString(), playerName);
+            room.TryAddPlayer(newPlayer);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.All.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
+
+            return room;
+        }
+
+        public async Task<GameRoom>? JoinRoomAsync(string roomId, string playerName)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+            if (room is not null)
+            {
+                var newPlayer = new Player(Context.ConnectionId, playerName);
+                if (room.TryAddPlayer(newPlayer))
+                {
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+                    await Clients.Group(roomId).SendAsync("PlayerJoined", newPlayer);
+                    return room;
+                }
+                    
+            }
+
+            return null;
+        }
+
+        public async Task StartGameAsync(string roomId)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+            if (room is not null) 
+            {
+                room.Game.StartGame();
+                await Clients.Group(roomId).SendAsync("UpdateGame", room);
+            }
+        }
+
+        public async Task MakeMove(string roomId, int row, int col, string playerId)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+            if (room is not null && room.Game.MakeMove(row, col, playerId))
+            {
+                room.Game.Winner = room.Game.CheckWinner();
+                room.Game.IsDraw = room.Game.CheckDraw() && string.IsNullOrEmpty(room.Game.Winner);
+
+                if (!string.IsNullOrEmpty(room.Game.Winner) || room.Game.IsDraw)
+                {
+                    room.Game.GameOver = true;
+                }
+
+                await Clients.Group(roomId).SendAsync("UpdateGame", room);
+            }
+        }
+    }
+}
